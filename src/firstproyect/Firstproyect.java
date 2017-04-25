0d1486c20d90ce81b412da8029292141f47c43f8
sena <sena@sena-MS-7817>
2017-04-25 06:21:15 -0500

    My first commit_

---
+++ src/firstproyect/Firstproyect.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstproyect;
+
+/**
+ *
+ * @author sena
+ */
+public class Firstproyect {
+    
+private int edad;
+public void setEdad(int edadParam)
+{
+    this.edad=edadParam;
+}
+public int getEdad()
+{
+    return this.edad;
+}
+
+
+private String nombre;
+public void setNombre(String nombreParam)
+{
+    this.nombre=nombreParam;
+}
+public String getNombre()
+{
+    return this.nombre;
+}
+    
+private int id;
+public void setId(int idParam)
+{
+    this.id=idParam;
+} 
+public int getId()
+{
+    return this.id;
+}
+private int tipoDocumento;
+public void setTipoDocumento(int tipoDocumentoparam)
+{
+    this.tipoDocumento=tipoDocumentoparam;
+}
+public int getTipoDodumento()
+{
+    return this.tipoDocumento;
+}
+        
+
+        
+}
